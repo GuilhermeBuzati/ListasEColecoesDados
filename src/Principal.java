6ef26f3c6975cbf8b96002aed367b0e8714ee4c9
Guilherme Buzati <guilhermeleopoldinoo@gmail.com>
2025-06-29 22:20:48 -0300

    init

---
+++ src/Principal.java
@@ -0,0 +1,46 @@
+import br.com.alura.orientacaoObjetoJava.calculos.CalculadoraDeTempo;
+import br.com.alura.orientacaoObjetoJava.calculos.FiltroRecomendacao;
+import br.com.alura.orientacaoObjetoJava.modelos.Episodio;
+import br.com.alura.orientacaoObjetoJava.modelos.Filme;
+import br.com.alura.orientacaoObjetoJava.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(2);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2010);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        lost.setMinutosPorEpisodios(50);
+        System.out.println(lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+    }
+}
